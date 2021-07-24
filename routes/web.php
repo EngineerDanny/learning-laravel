bc46a8aecb8873fde84bfb9c3769f6c6bd6604cf
EngineerDanny <agyapongdaniel7777@gmail.com>
2021-07-23 20:05:46 -0400

    testing router

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/x', function () {
+    printf("hello world u gotta miss me");
+
+    return view('welcome');
+});
