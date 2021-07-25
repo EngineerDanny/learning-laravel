d2391604849d858cba582fcc5349b0808b745be5
EngineerDanny <agyapongdaniel7777@gmail.com>
2021-07-24 20:56:05 -0400

    make controller to perform basic CRUD operations

--- database/migrations/2021_07_24_230433_create_products_table.php
+++ database/migrations/2021_07_24_230433_create_products_table.php
@@ -15,6 +15,10 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->decimal('price', 5, 2);
+            $table->string('url');
             $table->timestamps();
         });
     }
