d6627a9afece27f49595a5e0e63683ef668ef950
EngineerDanny <agyapongdaniel7777@gmail.com>
2021-08-05 08:15:27 -0400

    add user validation to auth controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -11,7 +12,20 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         //create a new user into the db
-        return User::create($request->all());
+
+        $fields = $request->validate([
+            'name' => 'required',
+            'email' => 'required|unique:users,email|max:50',
+            'password' => 'required',
+        ]);
+
+        $user =  User::create([
+            "name" => $fields["name"],
+            "email" => $fields["email"],
+            "password" => Hash::make($fields["password"]),
+        ]);
+
+        return  response($user, 200);
 
         //return the jwt token
     }
