a6ee5eb8b55b7f5cda02c5605a1e3f60ed2fda0f
EngineerDanny <agyapongdaniel7777@gmail.com>
2021-08-05 10:00:12 -0400

    code refactor

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
-
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,7 +13,7 @@ use App\Http\Controllers\ProductController;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::get('/products', [ProductController::class, 'index']);
 Route::post('/product', [ProductController::class, 'store']);
