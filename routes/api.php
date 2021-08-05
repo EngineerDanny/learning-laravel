57880e4137844bf6898990eb1ed14505a530f20a
EngineerDanny <agyapongdaniel7777@gmail.com>
2021-08-05 11:10:27 -0400

    logout route

--- routes/api.php
+++ routes/api.php
@@ -26,4 +26,5 @@ Route::middleware('auth:sanctum')->group(function () {
 Route::prefix('auth')->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
